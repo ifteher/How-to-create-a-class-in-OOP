1169c7ef1451cfde5acde04b0426e9e2de3985d0
Mohammad ifteher uddin <135147713+ifteher@users.noreply.github.com>
2024-05-28 23:57:29 +0600

    Add files via upload

---
+++ OOP create class.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class youtubechannel
+{
+public:
+    string Name;
+    string OwnerName;
+    int subscriberCount;
+    list<string>publishedVideoTitles;
+};
+int main()
+{
+    youtubechannel ytchannel;
+    ytchannel.Name = "ZayedOfficials";
+    ytchannel.OwnerName = "Zayed";
+    ytchannel.subscriberCount = 1800;
+    ytchannel.publishedVideoTitles = {"C++ for beginners vedio:1","HTML & CSS vedio:1" , "C++ OOP vedio:1"};
+    cout<<"Name: "<<ytchannel.Name<<endl;
+    cout<<"OwnerName: "<<ytchannel.OwnerName<<endl;
+    cout<<"subscriberCount: "<<ytchannel.subscriberCount<<endl;
+    cout<<"publishedVideoTitles are : "<<endl<<endl;
+
+    for(auto vedioTitle : ytchannel.publishedVideoTitles)
+    {
+        cout<<vedioTitle<<endl;
+    }
+    return 0;
+}
